b2228487b7196becc0e0995a20ac506bd771f833
aliciamorillo <63121998+aliciamorillo@users.noreply.github.com>
2021-09-15 08:30:32 +0200

    Delete 15. Name and age.py

--- 15. Name and age.py
+++
@@ -1,9 +0,0 @@
-#Please write a program which asks the user for their name and year of birth.
-# The program then prints out a message as follows:
-
-name = str(input("What is your name? "))
-year = int(input("Which year were you born? "))
-
-futureYears = 2021 - year
-
-print(f"Hi {name}, you will be {futureYears} years old at the end of the year 2021")
