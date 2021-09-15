2dcf5f820b277acfb9dd36f16453641cdee1c0d3
aliciamorillo <63121998+aliciamorillo@users.noreply.github.com>
2021-09-15 08:24:23 +0200

    Delete 03. Row, Row, Row Your Boat.py

--- 03. Row, Row, Row Your Boat.py
+++
@@ -1,6 +0,0 @@
-#Please write a program which prints out the following lines exactly as they are written 
-#here, punctuation and all:
-print("Row, row, row your boat,")
-print("Gently down the stream.")
-print("Merrily, merrily, merrily, merrily,")
-print("Life is but a dream.")
