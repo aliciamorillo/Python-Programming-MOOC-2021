fa435ea194f2b89481ffe491951bed40ac23a201
aliciamorillo <63121998+aliciamorillo@users.noreply.github.com>
2021-09-30 07:21:32 +0200

    Add files via upload

---
+++ Part 3/01. Print numbers.py
@@ -0,0 +1,6 @@
+number = 2
+
+while number < 31:
+    if number % 2 == 0:
+        print(number)
+    number += 1
---
+++ Part 3/03. Numbers.py
@@ -0,0 +1,6 @@
+limit = int(input("Upper limit:"))
+number = 0
+
+while number < limit-1:
+    number += 1
+    print(number)
